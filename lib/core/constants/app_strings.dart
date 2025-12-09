4176abe68f84ae563b4b2c1248f041cc2eb768f8
Sagaralam70 <sagaralammm@gmail.com>
2025-12-09 15:05:52 -0800

    Custom Container design

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -2,5 +2,6 @@ class AppStrings {
   static const login = "Login";
   static const email = "Email";
   static const password = "Password";
-  static const loginSuccess = "LoginSuccessful!";
+  static const loginSuccess = "Login Successful!";
+  static const loginPage = "Login Page";
 }
--- lib/widgets/custom_text_field/custom_text_field.dart
+++ lib/widgets/custom_text_field/custom_text_field.dart
@@ -23,7 +23,7 @@ class CustomTextField extends StatelessWidget {
         decoration: InputDecoration(
             hintText: hint,
             border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
+              borderRadius: BorderRadius.circular(12),
             ),
             ),
         );
